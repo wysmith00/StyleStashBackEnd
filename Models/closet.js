71db282b6b647597fb577b2e8f52c3c0b70ffb04
Kbaltazar55555 <kenneth.baltazarv@gmail.com>
2024-01-23 09:08:29 -0500

    added closet cont,model,route

--- Models/closet.js
+++ Models/closet.js
@@ -1,2 +1,22 @@
 
+//UserID - references Wyatt 'User' collection linking each closet to a specific user
+//closetSchema = storing object Ids to the 'Item' model.  Each array will represent a different closet category
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema; 
+
+const closetSchema = new mongoose.Schema({
+    userID: {
+        type: Schema.Types.Objectid,
+        required: true, 
+        ref: 'User'
+    },
+    outerwear: [{ type: Schema.Types.ObjectId, ref: 'Item' }],
+    footwear: [{ type: Schema.Types.ObjecteId, ref: 'Item' }],
+    clothing: [{ type: Schema.Types.ObjectedId, ref: 'Item' }],
+    accessories: [{ type: Schema.Types.objeectedId, ref: 'Item' }]
+});
+
+module.exports = mongoose.model('Closet', closetSchema);
+
 
---
+++ Models/itemdetails.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    category: { type: String, required: true },
+    color: String,
+    type: String,
+    size: String,
+    brand: String,
+    wearCount: String,
+    notes: String
+});
+
+module.exports = mongoose.model('Item', itemSchema);
