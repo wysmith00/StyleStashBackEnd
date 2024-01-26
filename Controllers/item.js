51a7b5ed5f9e7e758f7eb4f3c9cec48cf2872a02
wysmith00 <wysmith00@gmail.com>
2024-01-26 14:25:43 -0500

    still working backend

---
+++ Controllers/item.js
@@ -0,0 +1,64 @@
+import ItemDetails from "../Models/item.js";
+
+// Get all items
+
+const getAllItems = async (req, res) => {
+    try {
+        const items = await ItemDetails.find({});
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+// Get an item by ID
+const getItem = async (req, res) => {
+    try {
+        const item = await ItemDetails.findById(req.params.id);
+        if (!item) {
+            return res.status(404).send('Item not found');
+        }
+        res.status(200).json(item);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+// Add a new item
+const addItem = async (req, res) => {
+    try {
+        const newItem = new ItemDetails(req.body);
+        await newItem.save();
+        res.status(201).json(newItem);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+// Update an item
+const updateItem = async (req, res) => {
+    try {
+        const updatedItem = await ItemDetails.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedItem) {
+            return res.status(404).send('Item not found');
+        }
+        res.status(200).json(updatedItem);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+// Delete an item
+const deleteItem = async (req, res) => {
+    try {
+        const item = await ItemDetails.findByIdAndDelete(req.params.id);
+        if (!item) {
+            return res.status(404).send('Item not found');
+        }
+        res.status(200).send('Item deleted successfully');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+export default { getItem, addItem, updateItem, deleteItem, getAllItems}
---
+++ Models/item.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema;
+
+const itemDetailsSchema = new Schema({
+    category: { type: String, required: true },
+    color: { type: String },
+    type: { type: String },
+    size: { type: String},
+    brand: { type: String },
+    wearCount: {type: String},
+    notes: {type: String}
+});
+
+const ItemDetails = mongoose.model('ItemDetails', itemDetailsSchema);
+export default ItemDetails;
+
